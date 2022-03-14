f1d80d26bb0a29190b717d98602d31d663261948
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-15 00:35:42 +0300

    while-foreach

---
+++ console-programlama/while-foreach/while-foreach/while-foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            char character = 'a';
+
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+
+            }
+
+            Console.Write("***************************");
+            string[] arabalar = { "BMW", "Mercedes", "Ford", "Honda" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
