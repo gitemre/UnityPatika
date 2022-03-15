2e3acc644262560342f32a90bb5f5ddad300601e
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-15 23:17:08 +0300

    diziler

---
+++ console-programlama/diziler/diziler/diziler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "Kedi", "Maymun", "At"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama : " + toplam / diziUzunlugu);
+        }
+    }
+}
