0364993863697c300e78670518dde16b1e56fc46
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-19 22:19:48 +0300

    overloading

---
+++ console-programlama/method-overloading/method-overloading/method-overloading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi = "999";
+            int outSayi;
+            bool sonuc = int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+
+            }
+            else
+            {
+                Console.WriteLine("Başarılı!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2, 3, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+
+            int ifade = 999;
+            instance.EkranaYazdır(Convert.ToString(ifade));
+            instance.EkranaYazdır(ifade);
+
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a , int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdır(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
