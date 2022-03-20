62c1bf4f87cac243f9035946ed5476cff0c46438
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-20 20:22:10 +0300

    soru_4 cevaplar

---
+++ console-programlama/odev1/soru_4/soru_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace soru_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümle gir");
+            string cumle = Console.ReadLine();
+            string[] kelimeler = cumle.Split(new char[] { '.', '?', '!', ' ', ',' });
+
+            Console.WriteLine("cümlede {0} kelime var", kelimeler.Length);
+            Console.WriteLine("cümlede {0} tane harf var", cumle.Length - kelimeler.Length + 1);
+        }
+    }
+}
