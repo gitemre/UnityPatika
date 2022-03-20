2617bc1dba31688faee61cfb6b8d0458c111e47e
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-20 20:21:32 +0300

    soru_3 cevaplar

---
+++ console-programlama/odev1/soru_3/soru_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            string[] kelimeler = new string[sayi];
+            Console.WriteLine("Lütfen girdiğiniz sayı kadar kelime giriniz");
+
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine("{0}. kelimeyi gir.", i + 1);
+                kelimeler[i] = Convert.ToString(Console.ReadLine());
+            }
+
+            Array.Reverse(kelimeler);
+            foreach (var item in kelimeler)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
