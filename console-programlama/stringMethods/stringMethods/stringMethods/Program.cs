e94739ca5d47ee1040d5e7be15c4627e3d331091
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-21 22:12:46 +0300

    stringmethods

---
+++ console-programlama/stringMethods/stringMethods/stringMethods/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace stringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hosgeldiniz!";
+            string degisken2 = "CSharp";
+            Console.WriteLine(degisken.Length);
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+            Console.WriteLine(string.Concat(degisken, " Merhaba"));
+
+            Console.WriteLine(degisken.CompareTo(degisken2));
+            Console.WriteLine(String.Compare(degisken,degisken2,true));
+            Console.WriteLine(String.Compare(degisken, degisken2, false));
+
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hosgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba!"));
+
+            Console.WriteLine(degisken.IndexOf("CS"));
+            Console.WriteLine(degisken.IndexOf("Emre"));
+
+            Console.WriteLine(degisken.Insert(0, "Merhaba! "));
+            Console.WriteLine(degisken.LastIndexOf("i"));
+
+            Console.WriteLine(degisken + degisken2.PadLeft(30));
+            Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+            Console.WriteLine(degisken.PadRight(50,'-') + degisken2);
+
+            Console.WriteLine(degisken.Remove(10));
+            Console.WriteLine(degisken.Remove(5,3));
+            Console.WriteLine(degisken.Remove(0, 1));
+
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            Console.WriteLine(degisken.Replace(" ", "*"));
+
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4,6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
