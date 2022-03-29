f2d98d255fb945f7d06d19c7388850fde57c56fc
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-30 00:19:11 +0300

    soru3

---
+++ console-programlama/Odev-2/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümleyi girin");
+            string[] girilenCumle = Console.ReadLine().Split(' ');
+            string[] sesliHarfList = { "a", "e", "ı", "i", "o", "ö", "ü", "u" };
+            ArrayList sesliHarf = new ArrayList();
+
+            for (int i = 0; i < girilenCumle.Length; i++)
+            {
+                foreach (var item in sesliHarfList)
+                {
+                    if (girilenCumle[i].Contains(item))
+                    {
+                        sesliHarf.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine("Sesli Harfler");
+            foreach (var harf in sesliHarf)
+            {
+                Console.WriteLine(harf);
+            }
+        }
+    }
+}
