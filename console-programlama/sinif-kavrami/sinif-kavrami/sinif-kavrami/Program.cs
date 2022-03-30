3d51b675f6a166024476825b8c4871d19392018d
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-03-30 20:27:08 +0300

    sinif-kavrami

---
+++ console-programlama/sinif-kavrami/sinif-kavrami/sinif-kavrami/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.ad = "Emre";
+            calisan1.soyad = "Öztürk";
+            calisan1.no = 403;
+            calisan1.departman = "QA";
+
+            calisan1.calisanBilgiler();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.ad = "Ahmet";
+            calisan2.soyad = "Öztürk";
+            calisan2.no = 563;
+            calisan2.departman = "Dev";
+
+            calisan2.calisanBilgiler();  
+        }
+    }
+
+    class Calisan
+    {
+        public string ad;
+        public string soyad;
+        public int no;
+        public string departman;
+        
+        public void calisanBilgiler()
+        {
+            Console.WriteLine("Çalışan adı:{0}", ad);
+            Console.WriteLine("Çalışan soyad:{0}", soyad);
+            Console.WriteLine("Çalışan no:{0}", departman);
+            Console.WriteLine("Çalışan departman:{0}", departman);
+
+        }
+    }
+}
